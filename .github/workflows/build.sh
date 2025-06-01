dfd2643beb60ee4cf20ecebb52452cc70c973f8c
Joel Guittet <joelguittet@gmail.com>
2025-06-01 22:33:24 +0200

    ci: update to ubuntu runnner 24.04 (#3)

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,7 +4,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2021-2023 joelguittet and c-list contributors
+# Copyright joelguittet and c-list contributors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-mkdir build
-cd build
-cmake -DENABLE_LIST_EXAMPLES=ON ..
-make -j$(nproc)
+cmake -B build -DENABLE_LIST_EXAMPLES=ON .
+cmake --build build -j$(nproc)
